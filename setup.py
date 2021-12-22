95ca7306c1cf9bf55ecbec09e1bbb12e83cab6e7
Dean LJ <d.laytonjames@gmail.com>
2021-12-22 22:10:39 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='py-challenge',
+    version='0.01',
+    packages=[''],
+    url='https://github.com/deanlj-dev/py_challenge',
+    license='MIT',
+    author='Dean Layton-James',
+    author_email='d.laytonjames@gmail.com',
+    description='Sum all seqlen fields in *.data.json'
+)
